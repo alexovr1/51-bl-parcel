4bbef5e3cedf0441d364c1a1a8e3453e859a2fe4
Krystyna Sharhorodska <rinjara@gmail.com>
2022-08-20 11:59:16 +0300

    task 2 done

--- src/js/galery.js
+++ src/js/galery.js
@@ -1,22 +1,48 @@
 // #BASE_URL = 'https://api.unsplash.com/search/photos';
 // #API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
 
-import { UnSplash } from "./onSplashApi";
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+import { UnSplash } from './onSplashApi';
 import { createGalleryCards } from '../templates/createGalleryCards';
 
 const refs = {
-    gallery: document.querySelector('.js-gallery')
-}
+  gallery: document.querySelector('.js-gallery'),
+  container: document.querySelector('#tui-pagination-container'),
+};
+const options = {
+  totalItems: 0,
+  itemsPerPage: 15,
+  visiblePages: 5,
+  page: 1,
+};
+
+const pagination = new Pagination(refs.container, options);
+
+const page = pagination.getCurrentPage();
+console.log(page);
 
 const apiService = new UnSplash();
 
-apiService.getPopularImage(1).then(({ results }) => {
+apiService
+  .getPopularImage(page)
+  .then(({ results, total }) => {
     const markUp = createGalleryCards(results);
-    console.log(markUp);
-    refs.gallery.innerHTML = markUp
+    pagination.reset(total);
+    refs.gallery.innerHTML = markUp;
+  })
+  .catch(error => console.log(error.message));
 
-})
+pagination.on('afterMove', updatePagination);
+
+function updatePagination(e) {
+  const currentPage = e.page;
+  apiService
+    .getPopularImage(currentPage)
+    .then(({ results }) => {
+      const markUp = createGalleryCards(results);
+      refs.gallery.innerHTML = markUp;
+    })
     .catch(error => console.log(error.message));
-
-
-
+}
